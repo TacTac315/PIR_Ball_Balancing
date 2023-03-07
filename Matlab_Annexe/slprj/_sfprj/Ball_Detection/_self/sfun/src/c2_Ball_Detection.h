8a19f5dd2a996e1a57fc130ae516db49111383f9
Vincent Eychenne <veychenn@etud.insa-toulouse.fr>
2023-03-07 13:22:42 +0100

    Normalisation des coordonnées de la balle

---
+++ Matlab_Annexe/slprj/_sfprj/Ball_Detection/_self/sfun/src/c2_Ball_Detection.h
@@ -0,0 +1,417 @@
+#ifndef __c2_Ball_Detection_h__
+#define __c2_Ball_Detection_h__
+
+/* Type Definitions */
+#ifndef struct_tag_2bRLMk8wiuCArlx1mtie6F
+#define struct_tag_2bRLMk8wiuCArlx1mtie6F
+
+struct tag_2bRLMk8wiuCArlx1mtie6F
+{
+  int32_T S0_isInitialized;
+  int16_T W0_N_PIXLIST_DW[230400];
+  int16_T W1_M_PIXLIST_DW[230400];
+  uint32_T W2_NUM_PIX_DW[50];
+  uint8_T W3_PAD_DW[232404];
+  uint32_T W4_STACK_DW[230400];
+  int32_T P0_WALKER_RTP[8];
+  uint32_T P1_MINAREA_RTP;
+  uint32_T P2_MAXAREA_RTP;
+};
+
+#endif                                 /*struct_tag_2bRLMk8wiuCArlx1mtie6F*/
+
+#ifndef typedef_c2_vision_BlobAnalysis_0
+#define typedef_c2_vision_BlobAnalysis_0
+
+typedef struct tag_2bRLMk8wiuCArlx1mtie6F c2_vision_BlobAnalysis_0;
+
+#endif                                 /*typedef_c2_vision_BlobAnalysis_0*/
+
+#ifndef struct_tag_hlxJbgIQhuam3NMqS24BgD
+#define struct_tag_hlxJbgIQhuam3NMqS24BgD
+
+struct tag_hlxJbgIQhuam3NMqS24BgD
+{
+  uint32_T f1[8];
+};
+
+#endif                                 /*struct_tag_hlxJbgIQhuam3NMqS24BgD*/
+
+#ifndef typedef_c2_cell_wrap_3
+#define typedef_c2_cell_wrap_3
+
+typedef struct tag_hlxJbgIQhuam3NMqS24BgD c2_cell_wrap_3;
+
+#endif                                 /*typedef_c2_cell_wrap_3*/
+
+#ifndef struct_tag_xKIpogdu09egxX802kLoRE
+#define struct_tag_xKIpogdu09egxX802kLoRE
+
+struct tag_xKIpogdu09egxX802kLoRE
+{
+  boolean_T matlabCodegenIsDeleted;
+  boolean_T tunablePropertyChanged[2];
+  int32_T isInitialized;
+  boolean_T isSetupComplete;
+  boolean_T TunablePropsChanged;
+  c2_cell_wrap_3 inputVarSize[1];
+  c2_vision_BlobAnalysis_0 cSFunObject;
+  real_T MinimumBlobArea;
+  real_T MaximumBlobArea;
+  boolean_T NoTuningBeforeLockingCodeGenError;
+};
+
+#endif                                 /*struct_tag_xKIpogdu09egxX802kLoRE*/
+
+#ifndef typedef_c2_visioncodegen_BlobAnalysis
+#define typedef_c2_visioncodegen_BlobAnalysis
+
+typedef struct tag_xKIpogdu09egxX802kLoRE c2_visioncodegen_BlobAnalysis;
+
+#endif                                 /*typedef_c2_visioncodegen_BlobAnalysis*/
+
+#ifndef struct_tag_b8xz23RQm1RC3VjNSCrBOH
+#define struct_tag_b8xz23RQm1RC3VjNSCrBOH
+
+struct tag_b8xz23RQm1RC3VjNSCrBOH
+{
+  char_T f1[15];
+  real_T f2;
+  char_T f3[15];
+  real_T f4;
+};
+
+#endif                                 /*struct_tag_b8xz23RQm1RC3VjNSCrBOH*/
+
+#ifndef typedef_c2_cell_4
+#define typedef_c2_cell_4
+
+typedef struct tag_b8xz23RQm1RC3VjNSCrBOH c2_cell_4;
+
+#endif                                 /*typedef_c2_cell_4*/
+
+#ifndef struct_tag_MMJmsWQJNYUd0miNZl73YC
+#define struct_tag_MMJmsWQJNYUd0miNZl73YC
+
+struct tag_MMJmsWQJNYUd0miNZl73YC
+{
+  char_T f1[15];
+  char_T f2[15];
+};
+
+#endif                                 /*struct_tag_MMJmsWQJNYUd0miNZl73YC*/
+
+#ifndef typedef_c2_s_MMJmsWQJNYUd0miNZl73YC
+#define typedef_c2_s_MMJmsWQJNYUd0miNZl73YC
+
+typedef struct tag_MMJmsWQJNYUd0miNZl73YC c2_s_MMJmsWQJNYUd0miNZl73YC;
+
+#endif                                 /*typedef_c2_s_MMJmsWQJNYUd0miNZl73YC*/
+
+#ifndef struct_tag_1cLa9LcSjPFVyTnqRkh4VB
+#define struct_tag_1cLa9LcSjPFVyTnqRkh4VB
+
+struct tag_1cLa9LcSjPFVyTnqRkh4VB
+{
+  char_T f1[5];
+  char_T f2[6];
+  char_T f3[6];
+  char_T f4[6];
+};
+
+#endif                                 /*struct_tag_1cLa9LcSjPFVyTnqRkh4VB*/
+
+#ifndef typedef_c2_cell_6
+#define typedef_c2_cell_6
+
+typedef struct tag_1cLa9LcSjPFVyTnqRkh4VB c2_cell_6;
+
+#endif                                 /*typedef_c2_cell_6*/
+
+#ifndef struct_tag_xzcDJVCDY5ygSyoXSlHfkH
+#define struct_tag_xzcDJVCDY5ygSyoXSlHfkH
+
+struct tag_xzcDJVCDY5ygSyoXSlHfkH
+{
+  char_T f1[4];
+};
+
+#endif                                 /*struct_tag_xzcDJVCDY5ygSyoXSlHfkH*/
+
+#ifndef typedef_c2_cell_wrap_7
+#define typedef_c2_cell_wrap_7
+
+typedef struct tag_xzcDJVCDY5ygSyoXSlHfkH c2_cell_wrap_7;
+
+#endif                                 /*typedef_c2_cell_wrap_7*/
+
+#ifndef struct_tag_R92EHBRJBtqsluQDZ25dkE
+#define struct_tag_R92EHBRJBtqsluQDZ25dkE
+
+struct tag_R92EHBRJBtqsluQDZ25dkE
+{
+  char_T f1[7];
+};
+
+#endif                                 /*struct_tag_R92EHBRJBtqsluQDZ25dkE*/
+
+#ifndef typedef_c2_cell_wrap_0
+#define typedef_c2_cell_wrap_0
+
+typedef struct tag_R92EHBRJBtqsluQDZ25dkE c2_cell_wrap_0;
+
+#endif                                 /*typedef_c2_cell_wrap_0*/
+
+#ifndef struct_tag_UX05GARjqsuR9QofOrwOSD
+#define struct_tag_UX05GARjqsuR9QofOrwOSD
+
+struct tag_UX05GARjqsuR9QofOrwOSD
+{
+  char_T f1[27];
+  char_T f2[21];
+  char_T f3[26];
+};
+
+#endif                                 /*struct_tag_UX05GARjqsuR9QofOrwOSD*/
+
+#ifndef typedef_c2_cell_8
+#define typedef_c2_cell_8
+
+typedef struct tag_UX05GARjqsuR9QofOrwOSD c2_cell_8;
+
+#endif                                 /*typedef_c2_cell_8*/
+
+#ifndef struct_tag_uo7PCp2pQzQ8uFrW43NfEH
+#define struct_tag_uo7PCp2pQzQ8uFrW43NfEH
+
+struct tag_uo7PCp2pQzQ8uFrW43NfEH
+{
+  char_T f1[28];
+  char_T f2[22];
+  char_T f3[35];
+  char_T f4[26];
+  char_T f5[36];
+  char_T f6[39];
+};
+
+#endif                                 /*struct_tag_uo7PCp2pQzQ8uFrW43NfEH*/
+
+#ifndef typedef_c2_cell_9
+#define typedef_c2_cell_9
+
+typedef struct tag_uo7PCp2pQzQ8uFrW43NfEH c2_cell_9;
+
+#endif                                 /*typedef_c2_cell_9*/
+
+#ifndef struct_tag_AF9oIJqGDr0Kstxao4iUJE
+#define struct_tag_AF9oIJqGDr0Kstxao4iUJE
+
+struct tag_AF9oIJqGDr0Kstxao4iUJE
+{
+  real_T f1[2];
+};
+
+#endif                                 /*struct_tag_AF9oIJqGDr0Kstxao4iUJE*/
+
+#ifndef typedef_c2_cell_wrap_2
+#define typedef_c2_cell_wrap_2
+
+typedef struct tag_AF9oIJqGDr0Kstxao4iUJE c2_cell_wrap_2;
+
+#endif                                 /*typedef_c2_cell_wrap_2*/
+
+#ifndef struct_tag_3WNj9ufA8sQmw6UhmdEAsB
+#define struct_tag_3WNj9ufA8sQmw6UhmdEAsB
+
+struct tag_3WNj9ufA8sQmw6UhmdEAsB
+{
+  c2_cell_4 _data;
+};
+
+#endif                                 /*struct_tag_3WNj9ufA8sQmw6UhmdEAsB*/
+
+#ifndef typedef_c2_s_3WNj9ufA8sQmw6UhmdEAsB
+#define typedef_c2_s_3WNj9ufA8sQmw6UhmdEAsB
+
+typedef struct tag_3WNj9ufA8sQmw6UhmdEAsB c2_s_3WNj9ufA8sQmw6UhmdEAsB;
+
+#endif                                 /*typedef_c2_s_3WNj9ufA8sQmw6UhmdEAsB*/
+
+#ifndef struct_tag_LEUCK8o7KkJKdFcf3MyGUF
+#define struct_tag_LEUCK8o7KkJKdFcf3MyGUF
+
+struct tag_LEUCK8o7KkJKdFcf3MyGUF
+{
+  c2_s_MMJmsWQJNYUd0miNZl73YC _data;
+};
+
+#endif                                 /*struct_tag_LEUCK8o7KkJKdFcf3MyGUF*/
+
+#ifndef typedef_c2_s_LEUCK8o7KkJKdFcf3MyGUF
+#define typedef_c2_s_LEUCK8o7KkJKdFcf3MyGUF
+
+typedef struct tag_LEUCK8o7KkJKdFcf3MyGUF c2_s_LEUCK8o7KkJKdFcf3MyGUF;
+
+#endif                                 /*typedef_c2_s_LEUCK8o7KkJKdFcf3MyGUF*/
+
+#ifndef struct_tag_DSOsCbkMJXmOomV3xOVirB
+#define struct_tag_DSOsCbkMJXmOomV3xOVirB
+
+struct tag_DSOsCbkMJXmOomV3xOVirB
+{
+  c2_cell_6 _data;
+};
+
+#endif                                 /*struct_tag_DSOsCbkMJXmOomV3xOVirB*/
+
+#ifndef typedef_c2_s_DSOsCbkMJXmOomV3xOVirB
+#define typedef_c2_s_DSOsCbkMJXmOomV3xOVirB
+
+typedef struct tag_DSOsCbkMJXmOomV3xOVirB c2_s_DSOsCbkMJXmOomV3xOVirB;
+
+#endif                                 /*typedef_c2_s_DSOsCbkMJXmOomV3xOVirB*/
+
+#ifndef struct_tag_e7jvQoFbvOwJ3sc92LdjpG
+#define struct_tag_e7jvQoFbvOwJ3sc92LdjpG
+
+struct tag_e7jvQoFbvOwJ3sc92LdjpG
+{
+  c2_cell_wrap_7 _data;
+};
+
+#endif                                 /*struct_tag_e7jvQoFbvOwJ3sc92LdjpG*/
+
+#ifndef typedef_c2_s_e7jvQoFbvOwJ3sc92LdjpG
+#define typedef_c2_s_e7jvQoFbvOwJ3sc92LdjpG
+
+typedef struct tag_e7jvQoFbvOwJ3sc92LdjpG c2_s_e7jvQoFbvOwJ3sc92LdjpG;
+
+#endif                                 /*typedef_c2_s_e7jvQoFbvOwJ3sc92LdjpG*/
+
+#ifndef struct_tag_DyxoIdtJXNTXJqriOfucUD
+#define struct_tag_DyxoIdtJXNTXJqriOfucUD
+
+struct tag_DyxoIdtJXNTXJqriOfucUD
+{
+  c2_cell_wrap_0 _data;
+};
+
+#endif                                 /*struct_tag_DyxoIdtJXNTXJqriOfucUD*/
+
+#ifndef typedef_c2_s_DyxoIdtJXNTXJqriOfucUD
+#define typedef_c2_s_DyxoIdtJXNTXJqriOfucUD
+
+typedef struct tag_DyxoIdtJXNTXJqriOfucUD c2_s_DyxoIdtJXNTXJqriOfucUD;
+
+#endif                                 /*typedef_c2_s_DyxoIdtJXNTXJqriOfucUD*/
+
+#ifndef struct_tag_6khVZLlzg3270lst7fcoXG
+#define struct_tag_6khVZLlzg3270lst7fcoXG
+
+struct tag_6khVZLlzg3270lst7fcoXG
+{
+  c2_cell_8 _data;
+};
+
+#endif                                 /*struct_tag_6khVZLlzg3270lst7fcoXG*/
+
+#ifndef typedef_c2_s_6khVZLlzg3270lst7fcoXG
+#define typedef_c2_s_6khVZLlzg3270lst7fcoXG
+
+typedef struct tag_6khVZLlzg3270lst7fcoXG c2_s_6khVZLlzg3270lst7fcoXG;
+
+#endif                                 /*typedef_c2_s_6khVZLlzg3270lst7fcoXG*/
+
+#ifndef struct_tag_Q9exWnM4P2vFrlJou54v4D
+#define struct_tag_Q9exWnM4P2vFrlJou54v4D
+
+struct tag_Q9exWnM4P2vFrlJou54v4D
+{
+  c2_cell_9 _data;
+};
+
+#endif                                 /*struct_tag_Q9exWnM4P2vFrlJou54v4D*/
+
+#ifndef typedef_c2_s_Q9exWnM4P2vFrlJou54v4D
+#define typedef_c2_s_Q9exWnM4P2vFrlJou54v4D
+
+typedef struct tag_Q9exWnM4P2vFrlJou54v4D c2_s_Q9exWnM4P2vFrlJou54v4D;
+
+#endif                                 /*typedef_c2_s_Q9exWnM4P2vFrlJou54v4D*/
+
+#ifndef struct_tag_i3Yy1eMafhQ5Hb1YroYw5E
+#define struct_tag_i3Yy1eMafhQ5Hb1YroYw5E
+
+struct tag_i3Yy1eMafhQ5Hb1YroYw5E
+{
+  c2_cell_wrap_2 _data;
+};
+
+#endif                                 /*struct_tag_i3Yy1eMafhQ5Hb1YroYw5E*/
+
+#ifndef typedef_c2_s_i3Yy1eMafhQ5Hb1YroYw5E
+#define typedef_c2_s_i3Yy1eMafhQ5Hb1YroYw5E
+
+typedef struct tag_i3Yy1eMafhQ5Hb1YroYw5E c2_s_i3Yy1eMafhQ5Hb1YroYw5E;
+
+#endif                                 /*typedef_c2_s_i3Yy1eMafhQ5Hb1YroYw5E*/
+
+#ifndef typedef_SFc2_Ball_DetectionInstanceStruct
+#define typedef_SFc2_Ball_DetectionInstanceStruct
+
+typedef struct {
+  SimStruct *S;
+  ChartInfoStruct chartInfo;
+  int32_T c2_sfEvent;
+  boolean_T c2_doneDoubleBufferReInit;
+  uint8_T c2_is_active_c2_Ball_Detection;
+  uint8_T c2_JITStateAnimation[1];
+  uint8_T c2_JITTransitionAnimation[1];
+  int32_T c2_IsDebuggerActive;
+  int32_T c2_IsSequenceViewerPresent;
+  int32_T c2_SequenceViewerOptimization;
+  void *c2_RuntimeVar;
+  boolean_T c2_isInitialized;
+  uint32_T c2_mlFcnLineNumber;
+  void *c2_fcnDataPtrs[14];
+  char_T *c2_dataNames[14];
+  uint32_T c2_numFcnVars;
+  uint32_T c2_ssIds[14];
+  uint32_T c2_statuses[14];
+  void *c2_outMexFcns[14];
+  void *c2_inMexFcns[14];
+  real32_T c2_frame[691200];
+  real32_T c2_b_frame[691200];
+  real32_T c2_hsv[691200];
+  c2_visioncodegen_BlobAnalysis c2_blobAnalyzer;
+  boolean_T c2_mask[230400];
+  boolean_T c2_bv[230400];
+  boolean_T c2_varargin_1[230400];
+  CovrtStateflowInstance *c2_covrtInstance;
+  void *c2_fEmlrtCtx;
+  real_T (*c2_ballX_data)[2];
+  int32_T *c2_ballX_sizes;
+  real_T (*c2_ballY_data)[2];
+  int32_T *c2_ballY_sizes;
+  real32_T (*c2_c_frame)[691200];
+  int32_T (*c2_ballArea_data)[2];
+  int32_T *c2_ballArea_sizes;
+} SFc2_Ball_DetectionInstanceStruct;
+
+#endif                                 /*typedef_SFc2_Ball_DetectionInstanceStruct*/
+
+/* Named Constants */
+
+/* Variable Declarations */
+
+/* Variable Definitions */
+
+/* Function Declarations */
+extern const mxArray *sf_c2_Ball_Detection_get_eml_resolved_functions_info(void);
+
+/* Function Definitions */
+extern void sf_c2_Ball_Detection_get_check_sum(mxArray *plhs[]);
+extern void c2_Ball_Detection_method_dispatcher(SimStruct *S, int_T method, void
+  *data);
+
+#endif
