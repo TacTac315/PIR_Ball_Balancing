71fcd271dbd794876794650e46dfb83a54be6ec5
Vincent Eychenne <veychenn@etud.insa-toulouse.fr>
2023-05-09 10:39:33 +0200

    Ajout modele simulation correcteur + maj data.mat @Heitor

---
+++ dataToLookupTable.m
@@ -0,0 +1,14 @@
+%Ce programme à pour fonction de : 
+% - Charger le fichier de correspondance AnglePlateau->AngleServos généré
+%   grâce au programme Python de Johan Link
+% - Convertir les données en Lookup Table afin de les rendres exploitables
+%   par Simulink
+load('data.mat');
+xrange = min(data.alpha):0.2:max(data.alpha);
+yrange = min(data.beta):0.2:max(data.beta);
+
+[Alpha,Beta]=meshgrid(xrange,yrange);
+
+AValues=griddata(data.alpha,data.beta,data.AngleservoA,Alpha,Beta);
+BValues=griddata(data.alpha,data.beta,data.AngleservoB,Alpha,Beta);
+CValues=griddata(data.alpha,data.beta,data.AngleservoC,Alpha,Beta);
